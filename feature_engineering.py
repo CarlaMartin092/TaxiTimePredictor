72185b835e1bffd31f2f10b4340e126400f94c72
CarlaMartin092 <carlamartin092@gmail.com>
2021-02-10 09:06:18 +0100

    Dropped rows with missing distance values, added MAE function to class regression

--- feature_engineering.py
+++ feature_engineering.py
@@ -13,25 +13,28 @@ def feature_engineering(include_dummies = False):
     taxitime_weather["Hour"] = pd.to_datetime(taxitime_weather["Flight Datetime"]).dt.hour
     taxitime_weather["morning_mode"] = taxitime_weather['Hour'].apply(lambda h: 1 if (h >= 5) and (h <= 18) else 0)
 
-    taxitime_weather['Distance_proxy_m'] = taxitime_weather.groupby(['Aircraft Model','Hour', 'Weekday', 'Month', 'Year'])['Distance_proxy_m'].transform(lambda x: x.fillna(x.mode()) if not x.mode().empty else taxitime_weather['Distance_proxy_m'].mean())
+    print(np.sum(pd.isna(taxitime_weather['Distance_proxy_m'])))
+    #taxitime_weather['Distance_proxy_m'] = taxitime_weather.groupby(['Aircraft Model','Hour', 'Weekday', 'Month', 'Year'])['Distance_proxy_m'].transform(lambda x: x.fillna(x.mode()) if not x.mode().empty else taxitime_weather['Distance_proxy_m'].mean())
+    taxitime_weather = taxitime_weather.dropna(axis = 0, subset = ["Distance_proxy_m"])
+    print(np.sum(pd.isna(taxitime_weather['Distance_proxy_m'])))
     taxitime_weather['Log_distance_m'] = np.log(taxitime_weather['Distance_proxy_m'])
     #Drop useless columns
     taxitime_weather = taxitime_weather.drop(["Flight Number", "Flight Datetime", "Aircraft Model", "Aircraft Length", "Aircraft Span", "No. Engines", \
         "Airport Arrival/Departure", "Distance_proxy_m", "Movement Type", "AOBT", "ATOT", "time_hourly", 'apparentTemperature', 'dewPoint',\
-        'humidity', 'pressure', 'windGust', 'windBearing', 'cloudCover', 'precipProbability', 'ozone', 'uvindex'], axis = 1)
+        'humidity', 'pressure', 'windGust', 'windBearing', 'cloudCover', 'precipProbability', 'ozone', 'uvIndex', 'Hour'], axis = 1)
 
     #print("Weather cleaning start ----- ")
     #Weather data cleaning:
     taxitime_weather.loc[:,['summary', 'precipIntensity',\
-       'temperature', 'windSpeed', 'visibility', 'precipType']] = taxitime_weather.loc[:,['summary', 'precipIntensity',\
-       'temperature', 'windSpeed', 'visibility', 'precipType']].fillna(method = "ffill")
+       'temperature', 'windSpeed', 'visibility', 'precipType', 'precipAccumulation']] = taxitime_weather.loc[:,['summary', 'precipIntensity',\
+       'temperature', 'windSpeed', 'visibility', 'precipType', 'precipAccumulation']].fillna(method = "ffill")
 
-    print(sum(pd.isna(taxitime_weather.summary)))
-    print(sum(pd.isna(taxitime_weather.precipType)))
+    #print(sum(pd.isna(taxitime_weather.summary)))
+    #print(sum(pd.isna(taxitime_weather.precipType)))
 
     taxitime_weather["night"] = taxitime_weather.icon.apply(lambda summ: 1 if "night" in str(summ) else 0)
     taxitime_weather["y"] = taxitime_weather["actual_taxi_out_sec"]
-    taxitime_weather = taxitime_weather.drop(['icon', 'actual_taxi_out_sec', 'Hour'], axis = 1)
+    taxitime_weather = taxitime_weather.drop(['icon', 'actual_taxi_out_sec'], axis = 1)
     #print("Weather cleaning completed ----- ")
 
     if(include_dummies):
@@ -46,10 +49,10 @@ def feature_engineering(include_dummies = False):
 
     print(taxitime_weather.columns)
     print("Number of columns: ", len(taxitime_weather.columns))
-    #pd.to_csv("../data/")
+    taxitime_weather.to_csv("../data/taxitime_variables.csv")
     return(taxitime_weather)
 
-feature_engineering()
+#feature_engineering()
 
 
 
--- regression.py
+++ regression.py
@@ -1,29 +1,94 @@
 import pandas as pd
+import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import mean_absolute_error
-import feature_engineering
+from sklearn.metrics import mean_absolute_error, r2_score
+import feature_engineering as ft
+from scipy import stats
+import os
 
-def preprocess_for_reg(data, train_size = 0.7):
-    scaler = StandardScaler().fit(data)
-    data_scaled = scaler.transform(data)
-    X_train, y_train, X_test, y_test = train_test_split(data_scaled.drop(["y"]), data_scaled["y"], train_size = train_size, shuffle = False)
-    return(X_train, y_train, X_test, y_test, scaler)
+class Regression():
+    """
+    Multilinear regression class. Scales input data and performs multilinear regression with target variable "y". Allows to retrieve \
+    the regression coefficients.
+    """
+    def __init__(self):
+        """
+        Initializes scalers and sklearn LinearRegression object.
+        """
+        self.x_scaler, self.y_scaler = StandardScaler(), StandardScaler()
+        self.coef_ = []
+        self.p = []
+        self.r2 = 0.0
+        self.reg = LinearRegression()
+    
+    def preprocess(self, data, train_size = 0.7, random_state = None):
+        """
+        Rescales the input data by subtracting the mean and dividing by the standard deviation of each variable. Splits the data into 
+        training and testing data.
 
-def fit_regression(X_train, y_train):
-    reg = LinearRegression()
-    reg.fit(X_train, y_train)
-    return(reg)
+        Arguments:
+        - data: pandas dataframe, contains the independent variables and the dependent variable called "y"
+        - train_size: float, proportion of the data that is used for training
+        - random_state: int or None, sets a random seed
+        """
+        X = data.loc[:, data.columns != "y"]
+        y = data.loc[:,"y"]
+        self.x_scaler.fit(X)
+        self.y_scaler.fit(np.array(y).reshape(-1, 1))
+        X_rescaled = self.x_scaler.transform(X)
+        y_rescaled = self.y_scaler.transform(np.array(y).reshape(-1, 1))
+        X_train, X_test, y_train, y_test = train_test_split(X_rescaled, y_rescaled, train_size = train_size, random_state = random_state)
+        return(X_train, X_test, y_train, y_test)
 
-def assess_mae(data, train_size = 0.7):
-    X_train, y_train, X_test, y_test, scaler = preprocess_for_reg(data, train_size=0.7)
-    fitted_reg = fit_regression(X_train, y_train)
-    y_pred = fitted_reg.predict(X_test)
-    return(mean_absolute_error(y_pred, y_test))
+    def fit(self, X_train, y_train):
+        self.reg.fit(X_train, y_train)
+        self.coef_ = self.reg.coef_
 
-def predict_regression(reg, x):
-    return(reg.predict(x))
+        sse = np.sum((self.reg.predict(X_train) - y_train) ** 2, axis=0) / float(X_train.shape[0] - X_train.shape[1])
+        se = np.array([
+            np.sqrt(np.diagonal(sse[i] * np.linalg.inv(np.dot(X_train.T, X_train))))
+                                                    for i in range(sse.shape[0])])
+        self.t = self.coef_ / se
+        self.p = 2 * (1 - stats.t.cdf(np.abs(self.t), y_train.shape[0] - X_train.shape[1]))
+        return self
 
+    def predict(self, x):
+        x_rescaled = self.x_scaler.transform(x.reshape(1, -1))
+        y_pred = self.reg.predict(x_rescaled.reshape(1, -1))
+        y_pred_rescaled = self.y_scaler.inverse_transform(y_pred)
+        return(y_pred_rescaled)
+    
+    def evaluate(self, X_test, y_test):
+        X_test_rescaled = self.x_scaler.transform(X_test)
+        y_test_rescaled = self.y_scaler.transform(np.array(y_test).reshape(-1, 1))
+        y_pred = self.reg.predict(X_test_rescaled)
+        return(r2_score(y_test_rescaled, y_pred), mean_absolute_error(y_test_rescaled, y_pred))
+    
+    def get_coef_(self):
+        return(self.coef_)
+    def get_p_values(self):
+        return self.p
+
+def run_regression(include_dummies = False, train_size = 0.7, random_state = None):
+    if (os.path.exists("../data/taxitime_variables.csv") and not include_dummies):
+        taxitime_data = pd.read_csv("../data/taxitime_variables.csv")
+        taxitime_data = taxitime_data.drop(["Unnamed: 0"], axis = 1)
+
+    else: 
+        taxitime_data = ft.feature_engineering(include_dummies = include_dummies)
+
+    reg = Regression()
+    X_train, X_test, y_train, y_test = reg.preprocess(taxitime_data, train_size = train_size, random_state = random_state)
+    reg = reg.fit(X_train, y_train)
+    print("Coefficients: ", reg.get_coef_())
+    print("P-Values: ", reg.get_p_values())
+    r2, mae = reg.evaluate(X_test, y_test)
+    #print("R2 Score: ", r2)
+    print("MAE: ", mae)
+    print("Prediction for ", reg.x_scaler.inverse_transform(X_test[0,:]).T, ": ",  reg.predict(X_test[0,:]))
+
+#run_regression()
 
 
